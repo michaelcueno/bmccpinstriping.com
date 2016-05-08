7ee541145c125a8b58486719e62ca4dd75f247a6
Cueno, Michael <mcueno@amazon.com>
2016-05-08 09:24:56 -0700

    Make menu resize when fixed

--- js/siteNav.js
+++ js/siteNav.js
@@ -4,6 +4,13 @@ $(function() {
     var navHomeY = nav.offset().top;
     var isFixed = false;
     var $w = $(window);
+
+    $w.resize(function() {
+        if (!isFixed) {
+            navHomeY = nav.offset().top;
+        }
+    });
+
     $w.scroll(function() {
         var scrollTop = $w.scrollTop();
         var shouldBeFixed = scrollTop > navHomeY;
@@ -11,7 +18,8 @@ $(function() {
             nav.css({
                 position: 'fixed',
                 top: 0,
-                left: nav.offset().left,
+                left: 0,
+                right: 0,
                 width: nav.width()
             });
             isFixed = true;
